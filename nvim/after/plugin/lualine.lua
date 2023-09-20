44f851bb6469fe93b9aa8d2eb32c4e1458fb68bb
vaeep <diogobonofre@gmail.com>
2023-09-20 16:41:48 -0300

    Added tmux config latex config deleted lualine now shows open buffers, a better solution instead of using barbar

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -3,5 +3,10 @@ require('lualine').setup {
         theme = 'auto',
         section_separators = { left = ' 私 ', right = ' 很 ' },
         component_separators = { left = ' 私 ', right = ' 很 ' }
+    },
+    sections = {
+        lualine_a = {
+            'buffers',
+        }
     }
 }
--- nvim/lua/vaeep/remap.lua
+++ nvim/lua/vaeep/remap.lua
@@ -1,4 +1,6 @@
+local opt = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.api.nvim_set_keymap('n', '<leader>dt', [[:r!date '+\%d-\%m-\%Y'<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dt', [[:r!date '+\%d-\%m-\%Y'<CR>]], opt)
