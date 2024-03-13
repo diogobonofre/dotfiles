2f9deb26c6e065b8393e3b5669572fe506ff9493
Vaeep <diogobonofre@gmail.com>
2024-03-13 02:35:11 -0300

    minor mods in the terminal and vim colorscheme

--- nvim/lua/vaeep/packages/colorscheme.lua
+++ nvim/lua/vaeep/packages/colorscheme.lua
@@ -6,9 +6,9 @@ return {
 			vim.cmd([[:hi Normal guibg=#111111]])
 			vim.cmd([[:hi Pmenu guibg=#111111]])
 			vim.cmd([[:hi ColorColumn guibg=#1c1c1c]])
-			vim.api.nvim_set_hl(0, 'Comment', { italic = true })
-			vim.api.nvim_set_hl(0, 'Keyword', { italic = true, bold = true })
-			vim.api.nvim_set_hl(0, 'Variable', { italic = false })
+			--vim.api.nvim_set_hl(0, 'Comment', { italic = true })
+			--vim.api.nvim_set_hl(0, 'Keyword', { italic = true })
+			--vim.api.nvim_set_hl(0, 'Variable', { italic = false })
 		end
 	},
 }
