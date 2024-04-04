395f0aa5bf7d0c4909e77a83e2b2e72ad711226e
Vaeep <diogobonofre@gmail.com>
2024-04-04 14:23:16 -0300

    New Emacs configuration from scratch!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,129 +1,17 @@
-;; General
-(desktop-save-mode 1) ;; Save sessions
-
-;; Help with maintaining this file clean
-(setq custom-file "~/.emacs.d/autoconfig.el")
-(load custom-file)
-
-;; Save all Emacs temporary files (~, #) in one directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
-  kept-old-versions 5    ; and how many of the old
-  )
-
-;; Text Editor
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq indent-line-function 'insert-tab)
-
-;; Visual
-(scroll-bar-mode -1)                                  ; Display scroll bar
-(menu-bar-mode -1)                    				        ; Display menu bar
-(tool-bar-mode -1)                                    ; Display tool bar
-(global-display-line-numbers-mode)                    ; Enable line numbers
-(menu-bar--display-line-numbers-mode-relative)        ; Set relative line numbers
-;; Set font family, style, and size
-(set-face-attribute 'default nil
-                    :family "Iosevka Nerd Font Mono"	; Font family
-                    :height 250                       ; Font size (* 10)
-                    :weight 'medium                   ; Font weight: normal, bold, etc.
-                    :width 'normal)             			; Font width: normal, condensed, etc.
-(custom-set-faces					;; Customized fonts
- '(font-lock-comment-face ((t (:slant italic))))
- '(font-lock-keyword-face ((t (:slant italic)))))
-(load-theme 'plateau)
-	      
-;; Enable Electric Pair
-(electric-pair-mode 1)
-(setq electric-pair-pairs '((?\( . ?\))
-			    (?\[ . ?\])
-			    (?\{ . ?\})
-			    (?\" . ?\")
-			    (?\' . ?\')))
-
-;; Packages
-;; Set package archives
-(require 'package)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("marmalade" . "https://marmalade-repo.org/packages/")))
-
-;; Initialize packages
-(package-initialize)
-
-;; Install packages if they're not already installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; Set up use-package
-(eval-when-compile
-  (require 'use-package))
-(setq use-package-always-ensure t)
-
-;; Completion
-(use-package vertico
-  :ensure t
-  :custom
-  (vertico-cycle t)
-  :init
-  (vertico-mode))
-
-;; Save history between sessions
-(use-package savehist
-  :init
-  (savehist-mode))
-
-(setq history-length 100)
-
-;; Helps in corfu completions too
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-;; Give more information about files and dirs
-(use-package marginalia
-  :after vertico
-  :ensure t
-  :custom
-  (marginalia-annotators
-   '(marginalia-annotators-heavy
-     marginalia-annotators-light nil))
-  :init
-  (marginalia-mode))
-
-;; Configure directory extension.
-(use-package vertico-directory
-  :after vertico
-  :ensure nil
-  ;; More convenient directory navigation commands
-  :bind (:map vertico-map
-              ("RET" . vertico-directory-enter)
-              ("DEL" . vertico-directory-delete-char)
-              ("M-DEL" . vertico-directory-delete-word))
-  ;; Tidy shadowed file names
-  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
-
-(use-package nerd-icons
-  :ensure t
-  :custom
-  (nerd-icons-font-family "Iosevka NFP"))
-
-;; Discord Rich Presence
-(use-package elcord
-    :ensure t)
-
-;; Wakatime
-(use-package 'wakatime-mode)
-
-;; Themes
-(use-package autothemer
-    :ensure t)
-
-(use-package gruber-darker-theme)
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "e14884c30d875c64f6a9cdd68fe87ef94385550cab4890182197b95d53a7cf40" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "6e33d3dd48bc8ed38fd501e84067d3c74dfabbfc6d345a92e24f39473096da3f" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "2b501400e19b1dd09d8b3708cefcb5227fda580754051a24e8abf3aff0601f87" "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" "e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079" "b754d3a03c34cfba9ad7991380d26984ebd0761925773530e24d8dd8b6894738" "f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" "3ee898efcd3fa5b63c4f15e225f3616497010f2347a514490be8b563edbd39d9" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
