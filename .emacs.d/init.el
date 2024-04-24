84320297804e48b3430461b8dab06e4e0f418000
Vaeep <diogobonofre@gmail.com>
2024-04-24 05:42:40 -0300

    FIX: org-roam void uuid variable, theme confirmation on start Packages added: + org-roam-ui + rainbow-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,9 @@
+(require 'org)
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("23812ec0b33a27b03f4d71a35ab604fcaf54f5b64fc11d020dd061087e7a2a8e" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "9cda05ef581a03ce47f9e490c241f659520be02a7318757048db32fef4421da9" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,3 +11,11 @@
  ;; If there is more than one, they won't work right.
  '(line-number ((t (:inherit default :background "#000000" :foreground "#dddddd"))))
  '(line-number-current-line ((t (:inherit default :background "#000000" :foreground "#5f87af")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("691055ca72d9549e4bdfa77c3362e96fbe414a44baa56b4593e8d39d7b1ce55e" default))
+ '(warning-suppress-log-types '((emacs))))
