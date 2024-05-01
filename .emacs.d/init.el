1c28d50d29250535f5dd6b7fb1d3b85b33c4e818
Vaeep <diogobonofre@gmail.com>
2024-05-01 10:42:07 -0300

    changing system theme to solarized + minor mods on emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,3 @@
-(require 'org)
-
 (org-babel-load-file
  (expand-file-name
   "config.org"
@@ -9,13 +7,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(line-number ((t (:inherit default :background "#000000" :foreground "#dddddd"))))
- '(line-number-current-line ((t (:inherit default :background "#000000" :foreground "#5f87af")))))
+ '(line-number ((t (:inherit default))))
+ '(line-number-current-line ((t (:inherit default)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("691055ca72d9549e4bdfa77c3362e96fbe414a44baa56b4593e8d39d7b1ce55e" default))
- '(warning-suppress-log-types '((emacs))))
+   '("55c81b8ddb2b6c3fa502b1ff79fa8fed6affe362447d5e72388c7d160a2879d0" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" default))
+ '(warning-suppress-log-types '((emacs)))
+ '(warning-suppress-types '((use-package))))
--- nvim/lua/vaeep/packages/colorscheme.lua
+++ nvim/lua/vaeep/packages/colorscheme.lua
@@ -1,42 +1,12 @@
 return {
-	"casonadams/nord.vim",
-	"rose-pine/neovim",
-	"saulhoward/kaodam",
 	{
-		"mcchrish/zenbones.nvim",
-		dependencies = {
-			"tjdevries/colorbuddy.nvim",
-			"rktjmp/lush.nvim",
-		},
+		'maxmx03/solarized.nvim',
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd([[:colorscheme neobones]])
-			vim.cmd([[:hi Normal guibg=#121212]])
-			vim.cmd([[:hi Pmenu guibg=#121212]])
-			vim.cmd([[:hi ColorColumn guibg=#222222]])
-			vim.cmd([[:hi LineNr guibg=#121212]])
-			vim.cmd([[:hi LineNr guifg=#222222]])
-			vim.cmd([[:hi CursorLineNr guibg=#121212]])
-			vim.cmd([[:hi CursorLineNr guifg=#ebeeee]])
-			vim.cmd([[:hi Comment guifg=#444444]])
-			vim.cmd([[:hi Type guifg=#ffcc22]])
-			vim.cmd([[:hi Delimiter guifg=#ebeeee]])
-			vim.cmd([[:hi IncSearch guibg=#ffcc22]])
-			vim.cmd([[:hi IncSearch guifg=#121212]])
-			vim.cmd([[:hi Search guibg=#ebeeee]])
-			vim.cmd([[:hi Search guifg=#121212]])
-			vim.cmd([[:hi Error guifg=#888888]])
-			vim.cmd([[:hi MoreMsg guifg=#444444]])
-			vim.cmd([[:hi MatchParen guifg=#121212]])
-			vim.cmd([[:hi MatchParen guifg=#ffcc22]])
-			vim.cmd([[:hi NormalFloat guibg=#222222]])
-			vim.cmd([[:hi FloatBorder guibg=#ebeeee]])
-			vim.cmd([[:hi TelescopeNormal guibg=#121212]])
-			vim.cmd([[:hi TelescopeBorder guibg=#121212]])
-			vim.cmd([[:hi StatusLine guibg=#121212]])
-			vim.cmd([[:hi StatusLine guifg=#444444]])
-			--vim.api.nvim_set_hl(0, 'Comment', { italic = true })
-			--vim.api.nvim_set_hl(0, 'Keyword', { italic = true })
-			--vim.api.nvim_set_hl(0, 'Variable', { italic = false })
-		end
+			vim.o.background = 'dark' -- or 'light'
+
+			vim.cmd.colorscheme 'solarized'
+		end,
 	},
 }
