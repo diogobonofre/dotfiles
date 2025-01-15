295ea5368ea9a0f21bbbff3dc7f7d3edfd2e10cc
diogobonofre <diogobonofre@gmail.com>
2025-01-15 18:50:45 -0300

    emacs: added some new org mode configs and general QoL mods for text editing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,20 @@
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (load-theme 'modus-vivendi t)
+(global-visual-line-mode)
+(menu-bar--display-line-numbers-mode-relative)
+(global-display-line-numbers-mode t)
+(electric-pair-mode t)
+(setq electric-pair-delete-pair t)
 
-(require 'org)
-(setq org-clock-sound "~/Music/emacs-pomodoro-ding.wav")
+(use-package org
+  :ensure t
+  :config
+  (setq org-startup-indented t
+	org-hide-leading-stars t
+	org-clock-sound "~/Music/emacs-pomodoro-ding.wav"))
